0ebd88b0b1ddb835f3a99e635e7b1cad2629b9e5
Priyanshu Kumar Attri <princeattri2805@gmail.com>
2026-06-04 15:34:18 +0530

    Added Day4 problem 1

---
+++ Week_1/Day_4/Matrix_Diagonal_Sum.java
@@ -0,0 +1,24 @@
+package Week_1.Day_4;
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==j){
+                    sum+=mat[i][j];
+                }
+                if((i+j)==(n-1)){
+                    sum+=mat[i][j];
+                }
+            }
+        }
+       
+        if(n%2!=0){
+            sum-=mat[n/2][n/2];
+        }
+        return sum;
+    }
+}
