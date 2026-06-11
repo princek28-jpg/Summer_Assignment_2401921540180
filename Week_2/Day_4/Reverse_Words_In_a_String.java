f86b6c5ebd49b373a4827a04eba51c573f0cf633
Priyanshu Kumar Attri <princeattri2805@gmail.com>
2026-06-11 10:00:37 +0530

    Added Day4 problem 1

---
+++ Week_2/Day_4/Reverse_Words_In_a_String.java
@@ -0,0 +1,27 @@
+package Week_2.Day_4;
+
+class Solution {
+    private String reverse(int index,String[] st,StringBuilder sb){
+        if(st.length<=index){
+            return sb.toString();
+        }
+        char[] ch=st[index].toCharArray();
+        for(int i=0;i<ch.length/2;i++){
+            char temp=ch[i];
+            ch[i]=ch[ch.length-i-1];
+            ch[ch.length-i-1]=temp;
+        }
+        sb.append(new String(ch));
+
+        if(index<st.length-1){
+            sb.append(" ");
+        }
+        return reverse(index+1,st,sb);
+    }
+    public String reverseWords(String s) {
+        int n=s.length();
+        StringBuilder sb=new StringBuilder();
+        String[] st=s.split(" ");
+        return reverse(0,st,sb);
+    }
+}
