4ec0669fda786bc2a265b76da74458b3a02d11f8
Priyanshu Kumar Attri <princeattri2805@gmail.com>
2026-06-13 11:58:52 +0530

    Added

---
+++ Week_2/OOPs/music/string/Veena.java
@@ -0,0 +1,7 @@
+package Week_2.OOPs.music.string;
+import Week_2.OOPs.music.Playable;
+public class Veena implements Playable {
+    public void play(){
+        System.out.println("Playing Veena");
+    }
+}
