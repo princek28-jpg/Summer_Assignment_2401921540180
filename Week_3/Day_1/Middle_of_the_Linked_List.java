bc12162c93275bd68ba00d9645c583a67fd82476
Priyanshu Kumar Attri <princeattri2805@gmail.com>
2026-06-15 12:31:12 +0530

    Added problem 3

---
+++ Week_3/Day_1/Middle_of_the_Linked_List.java
@@ -0,0 +1,28 @@
+package Week_3.Day_1;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count=0;
+        ListNode temp=head;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        int mid=count/2;
+        while(mid>0){
+            head=head.next;
+            mid--;
+        }
+        return head;
+    }
+}
