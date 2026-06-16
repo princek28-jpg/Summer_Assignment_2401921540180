dfe669c4d1d7f5276105cec3053bc2f6635baf55
Priyanshu Kumar Attri <princeattri2805@gmail.com>
2026-06-16 14:09:45 +0530

    Added problem 3

---
+++ Week_3/Day_2/Palindrome_Linked_list.java
@@ -0,0 +1,34 @@
+package Week_3.Day_2;
+import java.util.ArrayList;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(head != null){
+            list.add(head.val);
+            head=head.next;
+        }
+
+        int l =0;
+        int r=list.size()-1;
+
+        while(l<r){
+            if(!list.get(l).equals(list.get(r))){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
