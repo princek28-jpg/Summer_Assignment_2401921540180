0cbc33e82100b914a6fa052ad3bfe904ea0cd8a5
Priyanshu Kumar Attri <princeattri2805@gmail.com>
2026-06-23 16:08:00 +0530

    Added problem 3

---
+++ Week_4/Day_2/Binary_tree_zigzag_level_order_traversal.java
@@ -0,0 +1,83 @@
+package Week_4.Day_2;
+import java.util.*;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> ans =
+            new ArrayList<>();
+
+        if(root == null){
+            return ans;
+        }
+
+        Queue<TreeNode> q =
+            new LinkedList<>();
+
+        q.add(root);
+
+        boolean reverse =
+            false;
+
+        while(!q.isEmpty()){
+
+            int size =
+                q.size();
+
+            List<Integer> level =
+                new ArrayList<>();
+
+            for(int i=0;i<size;i++){
+
+                TreeNode node =
+                    q.poll();
+
+                level.add(
+                    node.val
+                );
+
+                if(node.left!=null){
+                    q.add(
+                        node.left
+                    );
+                }
+
+                if(node.right!=null){
+                    q.add(
+                        node.right
+                    );
+                }
+            }
+
+            if(reverse){
+
+                Collections.reverse(
+                    level
+                );
+            }
+
+            ans.add(level);
+
+            reverse =
+                !reverse;
+        }
+
+        return ans;
+    }
+}
